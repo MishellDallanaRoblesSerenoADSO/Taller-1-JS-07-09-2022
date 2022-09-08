42284c5c4c46b25a1d1b0211aadec8ffd9575a7c
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-08 11:47:29 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 addEventListener("DOMContentLoaded",()=>{
-    // 9. Escribir un programa que calcule el área y el volumen de un cilindro:
-    // A  = (2 * (PI * r˄2)) + ((2 * PI * r) * h)
-    // V = (PI * r2) * h
-    let r= Number(prompt("Coloque el valor del radio del cilindro: ",5))
-    let h= Number(prompt("Coloque el valor de la altura del cilindro: ",8))
-    let resultado= Number(prompt("operacion   = (2 * (PI * r˄2)) + ((2 * PI * r) * h)", (2 * (math.PI) * r**2) + (2* (math.PI) * r) *h))
-    let rvolumen= Number(prompt("operacion   = (PI * r2) * h", ((math.PI) * r*2) * h))
-    console.log(`El area  del cilidndro es: ${resultado}`)
-    console.log(`El volumen del cilindro es: ${rvolumen}`)
+    // 10. Escribir un algoritmo que permita obtener las raíces reales de la ecuación de segundo grado: A * x2 +
+    // b * x + c, siendo X un valor constante.
+
+    let a= Number(prompt("Ingrese el valor de a: ",8))
+    let b= Number(prompt("Ingrese el valor de b: ",8))
+    let c= Number(prompt("Ingrese el valor de c: ",8))
+    let x= Number(prompt("coloque el valor de x: ",9))
+
+    let resultado= ((a*x)**2 + b* x *c)
+    console.log(`Las raices de la ecuacion es: ${resultado}`)
 })
