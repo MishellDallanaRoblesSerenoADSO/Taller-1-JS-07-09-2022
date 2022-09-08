e521cf34e828a4d1a24d9e5dafa90b26cf08b660
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-08 08:40:21 -0500

    Ejercicio 7

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 addEventListener("DOMContentLoaded",()=>{
-    // 6. Escribir un programa que calcule la velocidad de un proyectil que recorre 2 Km en 5 minutos. Expresar
-    // el resultado en metros/segundo. Velocidad = espacio/tiempo.
-    let espacio= Number(prompt("ingrese el valor del espacio: ",2000))
-    let tiempo= Number(prompt("ingrese el valor del tiempo: "))
-
-    let velocidad=(espacio*tiempo)
-    console.log(`La velocidad del proyectil es de: "${velocidad}", m/seg`)
+    // 7. Escribir un programa que calcule el volumen de una esfera:
+    // Radio = 3 volumen de la esfera = 4/3 * PI * radio˄3
+    let radio=Number(prompt("Ingrese el valor del radio: ",3))
+    let volumen= Number(prompt("Fórmula volumen de la esfera = 4/3 * PI * radio˄3", ((4/3)* (Math.PI) * (Math.pow )(radio,2)))) 
+    console.log(`El volumen de la esfera es: "${volumen}"`)
 })
