50891dad03034ff5227af2656c04b9e81810bf71
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-08 08:40:57 -0500

    Ejercicio 8

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 addEventListener("DOMContentLoaded",()=>{
-    // 7. Escribir un programa que calcule el volumen de una esfera:
-    // Radio = 3 volumen de la esfera = 4/3 * PI * radio˄3
-    let radio=Number(prompt("Ingrese el valor del radio: ",3))
-    let volumen= Number(prompt("Fórmula volumen de la esfera = 4/3 * PI * radio˄3", ((4/3)* (Math.PI) * (Math.pow )(radio,2)))) 
-    console.log(`El volumen de la esfera es: "${volumen}"`)
+    // 8.Escribir un programa que evalúe la siguiente expresión:
+    // (a+7*c)/(b+2-a)+2*b 
+    let a=Number(prompt("coloque el valor de a: "))
+    let b=Number(prompt("coloque el valor de b: "))
+    let c=Number(prompt("coloque el valor de c: "))
+
+    let ecuacion= Number(prompt("Ecuacion (a+7*c)/(b+2-a)+2*b", (a+7*c)/(b+2-a) +2*b))
+    console.log(`El valor de la ecuacion es: "${ecuacion}"`)
 })
