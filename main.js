c4ebe4d60d2b8ab57886f3c6503dc09859e26bf2
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-14 10:40:38 -0500

    Ejercicio 14

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 addEventListener("DOMContentLoaded",()=>{
-    //13. Realice un algoritmo que a partir de proporcionarle la velocidad de un automóvil expresada en
-    // kilómetros por hora, proporcione la velocidad en metros por segundos.
+    // 14. Una farmacia aplica al precio de los remedios el 10% de descuento, hacer un programa que
+    // ingresando el costo de los medicamentos calcules el descuento y el precio final.en metros por segundos.
 
+    // Precio del remedio y el valor del descuento que se le aplica
+    let remedio=Number(prompt("Coloque el valor del remedio: "))
+    let costo= (remedio * 0.10)
 
-    let cm= 355
-    let iva=0.20
-
-    let velocidad= Number(prompt("Coloque los km/h: ",))
-    let resultado=(velocidad *1000/3600)
-    console.log(`Velocidad en m/s: ${resultado}`)
+    console.log(`Descuento del remedio: ${costo}`)
+    // Valor total 
+    let valort=(remedio-costo)
+    console.log(`El valor total es: ${valort}`)
 })
