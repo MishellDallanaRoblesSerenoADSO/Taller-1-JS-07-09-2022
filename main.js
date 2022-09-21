b2e7c3f385066a45d8b7d4b2c59952200b5ca9a3
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-21 10:44:39 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 addEventListener("DOMContentLoaded",()=>{
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     // 1. Escribir un programa que sume, reste, multiplique y divida dos números
 
     let num1= Number(prompt("ingrese el numero 1", 2));
@@ -18,14 +19,22 @@ addEventListener("DOMContentLoaded",()=>{
     // descuento, realice un algoritmo que indique cuanto fue el valor a pagar por cada producto y el total
     // de la compra.
 >>>>>>> 1c926fb (Ejercicio 22)
+=======
+    // 23. Calcular la edad de una madre en el momento de dio a luz a alguno de sus hijos.
+    // • Generar el algoritmo que dé como resultado el tiempo estimado para el llenado de un
+    // depósito.
+    // • Q=V/t, siendo Q (caudal), V (volumen) y t (tiempo).
+    // • Volumen = PI *(radio^2)* H (altura del depósito)
+    // • El tiempo se encuentra en minutos.
+    // • Normalmente se mide el volumen en litros y el tiempo en segundos.
+>>>>>>> 55a7805 (Ejercicio 23)
 
-    // Costo de cada producto
-    let p1=Number(prompt("Valor primer producto: ",69))
-    let p2=Number(prompt("Valor segundo producto: ",45))
-    let p3=Number(prompt("Valor tercer producto: ",69))
-    let p4=Number(prompt("Valor cuarto producto: ",69))
-    let p5=Number(prompt("Valor quinto producto: ",69))
+    // datos de caudal, altura y radio del deposito
+    let c=Number(prompt("Cuál es el caudal en l/s: ",90))
+    let h=Number(prompt("Altura del deposito: ",13))
+    let r=Number(prompt("Radio del deposito: ",3))
     
+<<<<<<< HEAD
 <<<<<<< HEAD
     // operación
     let r=Number((x2-x1)**2+(y2-y1)**2)
@@ -47,4 +56,14 @@ addEventListener("DOMContentLoaded",()=>{
     console.log(`El valor del producto es: ${d4}`)
     console.log(`El valor del producto es: ${d5}`)
 >>>>>>> 1c926fb (Ejercicio 22)
+=======
+    // operación
+    let v= (Math.PI)*(r**2)*h
+    let t= v/c
+    let m= t/60      
+
+
+    console.log(`El tiempo en segundos que trada el deposito es de: ${t}`)
+    console.log(`El tiempo que demora llenando el deposito es de: ${m}`)
+>>>>>>> 55a7805 (Ejercicio 23)
 })
