4d40ad1e0e87cfd616805353c973979ac1fc684b
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-21 11:33:20 -0500

    Ejercicio 26

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-addEventListener("DOMContentLoaded",()=>{
-    // 25. En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial
-    // de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de
-    // nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para
-    // hacer el bizcocho es
+addEventListener("DOMContentLoaded", (e)=>{
+    // 26. Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg
+    // transporta ?
 
-    // cantidad de gramos
-    let gramos=Number(prompt("¿Cuántos gramos de cacao le aregará? ", 20))  
+    // cantidad kg
+    
+    let cajas = prompt("Cantidad de cajas que transporta", 25); 
 
     // operación
-    let t=gramos*10
+    let t = cajas*748
 
 
-    console.log(`La cantidad de gramos de harina que tiene que agregar es: ${t} gramos`)
+    console.log(`La cantidad que transporta es de: ${t} kg`)
 })
