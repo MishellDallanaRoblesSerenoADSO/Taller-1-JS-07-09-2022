deb2daf738e1790c81f9b98c0823a7df46e70dab
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-27 08:19:20 -0500

    Ejercicio 30

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 addEventListener("DOMContentLoaded", (e)=>{
-    //29. Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo,
-    // pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron. Realice el código
-    // que representen el algoritmo para solucionar este problema.
+    //30. Ingrese una frase que lo identifique como programador de Software y luego mostar esta frase
+    // invertida
 
-    // Solicita año de nacimiento y año actual
-    let a=Number(prompt("¿Cuál es su año de nacimiento?: ",5))
-    let ac=Number(prompt("Año actual: ",))
-    
-    // operación
-    let edad=ac-a
+    // frase
+    let f= prompt("Coloque la frase: ")
 
-    console.log(`La edad de la persona: ${edad}`)
+    console.log(`${f}`)
 
+    let finvertido =f.split("").reverse().join("")
+    console.log(`${finvertido}`)
 })
